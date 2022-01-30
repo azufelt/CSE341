be7fae681f80727e6731a5e98e508681cbb173e8
Ashley Zufelt <ashley.zufelt@gmail.com>
2022-01-29 23:28:04 -0500

    week 4

--- week4/app.js
+++ week4/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+// const PORT = process.env.PORT || 5000;
+
+const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -15,11 +18,26 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const corsOptions = {
+  origin: "https://backendnodeproject.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://azufelt:manager01@cluster0.ctdx0.mongodb.net/BackendNodeProject?retryWrites=true&w=majority";
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  //max's ID: '61f48c89414b826412e8c715'
+  User.findById('61f5fd73a22f3b8e0f556ac9')
     .then(user => {
       req.user = user;
       next();
@@ -34,14 +52,13 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
+    MONGODB_URL, options)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Ashley',
+          email: 'ashley.zufelt@gmail.com',
           cart: {
             items: []
           }
--- week4/controllers/shop.js
+++ week4/controllers/shop.js
@@ -46,7 +46,7 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    .execPopulate()
+    // .execPopulate()
     .then(user => {
       const products = user.cart.items;
       res.render('shop/cart', {
@@ -86,7 +86,12 @@ exports.postOrder = (req, res, next) => {
     .execPopulate()
     .then(user => {
       const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: { ...i.productId._doc } };
+        return {
+          quantity: i.quantity,
+          product: {
+            ...i.productId._doc
+          }
+        };
       });
       const order = new Order({
         user: {
@@ -107,7 +112,9 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ 'user.userId': req.user._id })
+  Order.find({
+      'user.userId': req.user._id
+    })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
